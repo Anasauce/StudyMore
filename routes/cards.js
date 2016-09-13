e05a713169a5a14c4607265ba1dcf7ecb70c2613
John Hopkins <johnthopkins@gmail.com>
2016-09-13 11:37:03 -0700

    refactored changes

--- routes/cards.js
+++ routes/cards.js
@@ -2,26 +2,24 @@ import express from 'express'
 import {Subject, Card} from '../database/db'
 const router = express.Router()
 
-/* GET home page. */
 router.get('/new', (request, response, next) => {
   const {id} = request.user
 
   Subject.findById(id)
-      .then(result => {
-        console.log(result);
-        response.render('create_card', { subject:  result })
-      }).catch(error => {
-        response.send({message: error})
-      })
+    .then(subject => {
+      response.render('create_card', { subject })
+    }).catch(error => {
+      response.send({message: error.message})
+    })
 })
 
 router.post('/create_card', ( request, response ) => {
-    const {id, front, back} = request.body
-    Card.create(front, back, id)
-      .then(result => {
-        response.redirect(`/subjects/${id}`)
-      })
-  })
+  const {id, front, back} = request.body
 
+  Card.create(front, back, id)
+    .then(result => {
+      response.redirect(`/subjects/${id}`)
+    })
+})
 
 module.exports = router
