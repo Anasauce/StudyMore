b747c017cff57e94088651d4ec56d7402f7868ba
John Hopkins <johnthopkins@gmail.com>
2016-09-13 11:25:55 -0700

    view page for subject dashboard and user dashboard now populates subjects and cards

--- database/db.js
+++ database/db.js
@@ -10,6 +10,8 @@ const findByLogin = 'SELECT * FROM users WHERE email=$1 AND password=$2'
 const createUser = 'INSERT INTO users( name, email, password ) VALUES ( $1, $2, $3 ) RETURNING id'
 const findSubjectById = 'SELECT * FROM subjects WHERE user_id=$1'
 const createCard = 'INSERT INTO cards(front, back, subject_id) VALUES ($1, $2, $3) RETURNING id'
+const findBySubjectId = 'SELECT * FROM subjects WHERE id=$1'
+const findCardBySubjectId = 'SELECT * FROM cards WHERE id=$1'
 
 const User = {
   findById: id => db.one( findUserById, [id] ),
@@ -21,11 +23,13 @@ const insertSubject = 'INSERT INTO subjects( title, user_id ) VALUES( $1, $2 ) R
 
 const Subject = {
   create: ( title, id ) => db.one( insertSubject, [ title, id ]),
-  findById: user_id => db.any( findSubjectById, [user_id])
+  findById: user_id => db.any( findSubjectById, [user_id]),
+  find: id => db.one( findBySubjectId, [id] )
 }
 
 const Card = {
-  create: (front, back, subject_id) => db.one(createCard, [front, back, subject_id])
+  create: (front, back, subject_id) => db.one(createCard, [front, back, subject_id]),
+  findBySubjectId: id => db.any( findCardBySubjectId, [id])
 }
 
 export { User, Subject, Card }
--- routes/subjects.js
+++ routes/subjects.js
@@ -1,7 +1,24 @@
 import express from 'express'
 const router = express.Router()
 
-import { Subject } from '../database/db'
+import { Subject, Card } from '../database/db'
+
+router.get('/:id', (request, response, next ) => {
+  const { id } = request.params
+  console.log( 'subject', id )
+
+  const query = [ Subject.find( id ), Card.findBySubjectId( id ) ]
+
+  Promise.all( query )
+    .then( result => {
+      const [ subject, cards ] = result
+      response.render( 'subjects', { subject, cards } )
+    })
+
+  Subject.find( id ).then( subject => {
+    response.render('subjects', { subject })
+  })
+})
 
 router.get('/new', (req, res, next) => {
   res.render('subjects/create')
